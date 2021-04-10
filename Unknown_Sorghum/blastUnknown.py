7a98b03cc63538d75c5cd6e47d83d3a7d2a536c6
Shel Burkes <shel.burkes@gmail.com>
2021-04-10 19:43:00 -0400

    Directory Cleanup

--- Unknown_Sorghum/blastUnknown.py
+++ Unknown_Sorghum/blastUnknown.py
@@ -28,6 +28,7 @@ db_name = "transposon_old_17453.fasta"
 LTR_array = ['LTR','Retrotransposon','gag', 'gypsy','Ty3', ' copia-like ', 'copia', 'gypsy-like', 'centromeric retrotranposon']
 DNA_array = ['transposon','Transposon', 'Transposable element','transposon-like', 'Rim2', 'CACTA', 'transposase', 'y1-rr', 'hAT', 'Anac', 'TNP2-like','transposable element', 'Mu tranposon']
 HEL_array = ['Helitron', 'helitron']
+ANY_array = LTR_array + DNA_array + HEL_array
 
 blastx_cline = NcbiblastxCommandline(cmd='blastn', query=input, out=ntpath.basename(input) +'_blastout.txt', outfmt='6 qseqid sseqid salltitles pident length mismatch gapopen qstart qend sstart send evalue bitscore', db=db_name, evalue=0.01)
 result_handle = blastx_cline()
@@ -58,9 +59,13 @@ for seq_record in SeqIO.parse(ntpath.basename(args.mfilename), 'fasta'):  # (gen
             count_Helitron+=1
             print(">" + seq_record.id.split("#")[0]  + "#RC/Helitron", file=sample)
             print(seq_record.seq, file=sample)
-        else:
+        elif "#Unknown" in df[0][0] and df[0][0] == seq_record.id and not any (e in df[0][2] for e in ANY_array)  == True:
             count_Unknown+=1
-            print(">" + seq_record.id.split("#")[0]  + "#Unknown", file=sample)
+            print(">" + seq_record.id, file=sample)
+            print(seq_record.seq, file=sample)
+            count_Unknown+=1
+        else:
+            print(">" + seq_record.id, file=sample)
             print(seq_record.seq, file=sample)
             continue  # Breaking here gives you only the best HSP.
 
