bd523a3656212c5a81d1e758ad5eb255291057fc
Shel Burkes <shel.burkes@gmail.com>
2021-09-01 12:33:59 -0400

    Update density.r

--- TIP_Analysis/density.r
+++ TIP_Analysis/density.r
@@ -1,5 +1,6 @@
-# Check if ggplot2 is installed, if so, load it. If not, install and load it
 
+
+# Check if ggplot2 is installed, if so, load it. If not, install and load it
 if("ggplot2" %in% rownames(installed.packages())){
   library(ggplot2)
 } else {
@@ -10,17 +11,16 @@ if("ggplot2" %in% rownames(installed.packages())){
 # Import a text file with gene positions
 # Column Headers: Chr, Strt (No end or Gene name required)
 
-genes <- read.table("Grif16309_density.bed",sep="\t",header=T)
+tips <- read.table("/Users/shelvasha/Grif16309/TEfinder_20210726224912/TE_density.bed",sep="\t",header=T)
+colnames(tips)[1:2] <- c("Chr","Start")
 
-# make sure the chromosomes are ordered in the way you want them to appear in the plot
+# Orders chromosomes to appear properly in the plot
+tips$Chr <- with(tips, factor(Chr, levels=paste("chr",c(1:22,"X","Y"),sep=""), ordered=TRUE))
 
-genes$chr <- with(genes, factor(chr, levels=paste("chr",c(1:22,"X","Y"),sep=""), ordered=TRUE))
-
-# make a density plot of genes over the provided chromosomes (or scaffolds ...)
-
-plottedGenes <- ggplot(genes) + geom_histogram(aes(x=pos),binwidth=1000000) + facet_wrap(~chr,ncol=2) + ggtitle("RefSeq genes density over human genome 19") + xlab("Genomic position (bins 1 Mb)") + ylab("Number of genes")
+# Cretes density plot of genes over the provided chromosomes (or scaffolds ...)
+plottedTips <- ggplot(tips) + geom_histogram(aes(x=Start),binwidth=1000000) + facet_wrap(~Chr,ncol=2) + ggtitle("TIPs density over S. bicolor genome") + xlab("Genomic position (bins 1 Mb)") + ylab("Number of TIPs")
+plottedTips
 
 # Save it to an image
 png("genes.png",width=1000,height=1500)
-print(plottedGenes)
-dev.off()
+print(plottedTips)
