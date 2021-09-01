cf2c304d0dc3cdbb40883cca45244ca45812939f
Shel Burkes <shel.burkes@gmail.com>
2021-09-01 12:48:20 -0400

    Added tip_density.png, updated density.r

--- TIP_Analysis/density.r
+++ TIP_Analysis/density.r
@@ -1,5 +1,3 @@
-
-
 # Check if ggplot2 is installed, if so, load it. If not, install and load it
 if("ggplot2" %in% rownames(installed.packages())){
   library(ggplot2)
@@ -8,19 +6,28 @@ if("ggplot2" %in% rownames(installed.packages())){
   library(ggplot2)
 }
 
-# Import a text file with gene positions
-# Column Headers: Chr, Strt (No end or Gene name required)
+# Check if circlize is installed, if so, load it. If not, install and load it
+if("circlize" %in% rownames(installed.packages())){
+  library(circlize)
+} else {
+  install.packages("circlize")
+  library(circlize)
+}
 
+
+# Import a text file with gene positions // Column Headers: Chr, Start (No end or Gene name required)
 tips <- read.table("/Users/shelvasha/Grif16309/TEfinder_20210726224912/TE_density.bed",sep="\t",header=T)
 colnames(tips)[1:2] <- c("Chr","Start")
+
 
 # Orders chromosomes to appear properly in the plot
 tips$Chr <- with(tips, factor(Chr, levels=paste("chr",c(1:22,"X","Y"),sep=""), ordered=TRUE))
 
-# Cretes density plot of genes over the provided chromosomes (or scaffolds ...)
-plottedTips <- ggplot(tips) + geom_histogram(aes(x=Start),binwidth=1000000) + facet_wrap(~Chr,ncol=2) + ggtitle("TIPs density over S. bicolor genome") + xlab("Genomic position (bins 1 Mb)") + ylab("Number of TIPs")
+
+# Creates density plot of genes over the provided chromosomes (or scaffolds ...)
+plottedTips <- ggplot(tips) + geom_histogram(aes(x=Start),binwidth=1000000) + facet_wrap(~Chr,ncol=2) + ggtitle("TIPs density over S. bicolor genome (1Mb bins)") + xlab("Genomic position (bins 1 Mb)") + ylab("Number of TIPs")
 plottedTips
 
 # Save it to an image
-png("genes.png",width=1000,height=1500)
+png("tips_density(1Mbin).png",width=1500,height=1000)
 print(plottedTips)
