28fa6402338dedc3c337a58bd752b10b23dffca8
Shel Burkes <shel.burkes@gmail.com>
2021-09-24 12:42:50 -0400

    Update reorder_BAM_directory.sh

--- TIP_Analysis/reorder_BAM_directory.sh
+++ TIP_Analysis/reorder_BAM_directory.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=Orion
+#SBATCH --job-name=reorderBAM_slurm_job
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=48:00:00
+
+cd /projects/cooper_research1/TERRA_BAP/
+mkdir -p /projects/cooper_research1/TERRA_BAP/BAM_by_ID/
+
 ## Get IDs of BAMs
-ls /projects/cooper_research1/TERRA_BAP/BAM_by_CHR/Chr01/*.bam | cut -d'/' -f7 | cut -d'.' -f1 > bamIDs.txt
+#ls /projects/cooper_research1/TERRA_BAP/BAM_by_CHR/Chr01/*.bam | cut -d'/' -f7 | cut -d'.' -f1 > bamIDs.txt
 
 ## Build BAM list to loop through
 bamARRAY=()
@@ -11,6 +22,6 @@ done
 while read p; do
     for ELEMENT in ${bamARRAY[@]}; do
         mkdir -p /projects/cooper_research1/TERRA_BAP/BAM_by_ID/${p}
-        cp ${ELEMENT}/${p}*.bam /projects/cooper_research1/TERRA_BAP/BAM_by_ID/${p}
+        cp -R -u -p ${ELEMENT}/${p}*.bam /projects/cooper_research1/TERRA_BAP/BAM_by_ID/${p}
     done
 done </projects/cooper_research1/TERRA_BAP/bamIDs.txt
